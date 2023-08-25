b60dcff27749573bc61571b1b604ae062ab4f229
Jerome Phelps (Jepp) <33312097+Cyberjepp@users.noreply.github.com>
2023-08-26 05:52:04 +1000

    Add checking for and unlocking of "Completionist"

---
+++ Completionist.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Threading;
+using MelonLoader;
+using Steamworks;
+using UnityEngine;
+
+namespace PDTA3D_AchievementFix
+{
+    public class Completionist : MelonMod
+    {
+        private const SteamGame.Achievement CompletionistAchievement = SteamGame.Achievement.ACH_UNINSTALL_ME;
+        private const float CheckDelayInSeconds = 5f;
+
+        public override void OnSceneWasInitialized(int buildIndex, string sceneName)
+        {
+            // Only check if game is ready.
+            if (Game.instance == null) return;
+            
+            LoggerInstance.Msg("Requesting current stats from Steam...");
+            SteamUserStats.RequestCurrentStats();
+            
+            // Delay check so Steam has time to respond.
+            Game.instance.StartCoroutine(DelayCheck());
+        }
+
+        private IEnumerator DelayCheck()
+        {
+            yield return new WaitForSeconds(CheckDelayInSeconds);
+            CheckRequirements();
+        }
+
+        private void CheckRequirements()
+        {
+            LoggerInstance.Msg("Checking requirements...");
+            
+            SteamGame component = Game.instance.GetComponent<SteamGame>();
+            for (var i = 0; i < component.Achievements.Length; i++)
+            {
+                var currentAchievement = (SteamGame.Achievement) i;
+                var isUnlocked = component.Achievements[i];
+                
+                // We don't need the Completionist achievement to be completed.
+                if (currentAchievement == CompletionistAchievement)
+                {
+                    if (isUnlocked)
+                    {
+                        LoggerInstance.Msg("You are already a Completionist - congratulations!");
+                        return;
+                    }
+                    
+                    continue;
+                }
+
+                if (!isUnlocked)
+                {
+                    LoggerInstance.Msg("There is more work to be done!");
+                    return;
+                }
+            }
+            
+            Game.instance.UnlockAchievement(CompletionistAchievement);
+            LoggerInstance.Msg("You are now a Completionist - congratulations!");
+        } 
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using PDTA3D_AchievementFix; // The namespace of your mod class
+// ...
+[assembly: MelonInfo(typeof(Completionist), "Completionist Achievement Fix", "1.0.0", "Jepp")]
+[assembly: MelonGame("Escalation Studios", "PDTA 3D")]
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PDTA3D_AchievementFix")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PDTA3D_AchievementFix")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0CE3CE88-4209-4E53-A659-D2A01ACBD300")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
